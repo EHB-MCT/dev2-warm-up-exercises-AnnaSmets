704e96ba6c5d50d5493d657fff234f4e46c04fe7
Anna Smets <anna.smets@student,ehb.be>
2025-02-20 16:00:18 +0100

    warmup 1

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-import {
-    getRandomScores,
-    getStudentJSON,
-    runTitle
-} from './lib.js';
+import { getRandomScores, getStudentJSON, runTitle } from "./lib.js";
 
-runTitle();
+let buttonName = document.getElementById("button-1a");
+let buttonBckg = document.getElementById("button-1b");
+let buttonReady = document.getElementById("button-1c");
+let content1 = document.getElementById("content-1");
+let sectie1 = document.getElementById("section-1");
 
 function warmup1() {
-    console.log("Exercise week 1");
+	console.log("Exercise week 1");
+	buttonName.onclick = function introduction() {
+		content1.innerHTML = "Hallo, dit is de oefening van Anna";
+	};
+	buttonBckg.onclick = function background() {
+		sectie1.style.backgroundColor = "lightblue";
+	};
+	buttonReady.onclick = function ready() {
+		let title = document.createElement("h1");
+		title.innerHTML = "Ik ben er klaar voor.";
+		content1.appendChild(title);
+	};
 }
 
 function warmup2() {
-    console.log("Exercise week 2");
-    const scores = getRandomScores(10);
+	console.log("Exercise week 2");
+	const scores = getRandomScores(10);
 }
 
 function warmup3() {
-    let student = getStudentJSON();
+	let student = getStudentJSON();
 }
 
 function warmup4() {
-    let main, min, max;
+	let main, min, max;
 }
 
-function warmup5() {
+function warmup5() {}
 
-}
+warmup1();
